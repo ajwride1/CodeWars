b3f01927d8afcaa5815fcbbbef9b6bfff8953b1d
Adam Wride <ajwride1@gmail.com>
2020-03-01 15:41:32 +0000

    new class created for removing vowels from a string

---
+++ CodeWars.Tests/DisemvowelTrolls.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Tests
+{
+    [TestClass]
+    public class DisemvowelTrolls
+    {
+        private const string ExpectedOutcome = "Ths wbst s fr lsrs LL!";
+        private const string Input = "This website is for losers LOL!";
+
+        [TestMethod]
+        public void ShouldRemoveAllVowels()
+        {
+            Assert.AreEqual(ExpectedOutcome, DisemvowellTrolls.Disemvowel(Input));
+        }
+
+        [TestMethod]
+        public void Alternate()
+        {
+            Assert.AreEqual(ExpectedOutcome,DisemvowellTrolls.Alternate(Input));
+        }
+    }
+}
---
+++ CodeWars/DisemvowellTrolls.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public static class DisemvowellTrolls
+    {
+        private static List<char> _Vowels = new List<char> { 'a', 'e', 'i', 'o', 'u', 'A','E','I','O','U' };
+        public static string Disemvowel(string str)
+        {
+            char[] characters = str.ToCharArray();
+
+            foreach (char character in characters.Where(c => c != ' ').Distinct())
+            {
+                if (_Vowels.Contains(character))
+                    str = str.Replace(character.ToString(), string.Empty);
+            }
+
+            return str;
+        }
+
+        public static string Alternate(string str)
+        {
+            return Regex.Replace(str, "[aeiou]", "", RegexOptions.IgnoreCase);
+        }
+    }
+}
